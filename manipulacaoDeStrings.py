14457830d1e12111f15d961a8447258883488c8e
Amadeu <amadeusantos31@gmail.con>
2022-08-14 13:48:31 -0300

    fazendo manipulação de strings no python

---
+++ manipulacaoDeStrings.py
@@ -0,0 +1,14 @@
+reneDescartes = "                    Daria tudo que sei pela metade do que ignoro.                "
+aristoleles = "   a EspEranÇa é o sOnhO do hOmeM acoRdAdo.    "
+socrates = "  COnHece-tE a tE mesMo e coNhEceRás O univERSo e oS deUSeS.   "
+
+print(reneDescartes)
+print("Colocar todas as letras para maiúsculas: " + reneDescartes.strip().upper())
+print()
+
+print(aristoleles)
+print("Colocado as inicial de todas as palavra maiúsculas: " + aristoleles.strip().title())
+print()
+
+print(socrates)
+print("Colocado so a primiera para maiúsculas e o resto para minúsculas: " + socrates.strip().capitalize())
